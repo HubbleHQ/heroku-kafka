c65bc90979f02cb739a4a0e26d389b5920812648
Lizzie <lizzie@hubblehq.com>
2018-01-04 17:12:08 +0000

    Pip setup file with MIT license and a manifest file

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
 from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    version='1.0',            # Update the version number for new releases
+    name='heroku-kafka',        # This is the name of your PyPI-package.
+    description='Python 2 & 3 kafka package for use with heroku\'s kafka.',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable'
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ],
+    url='https://github.com/HubbleHQ/heroku-kafka',
+    author='Hubble'
+    author_email='dev@hubblehq.com'
+    license='MIT',
+    scripts=['heroku_kafka']    # The name of your scipt, and also the command you'll be using for calling it
+)
