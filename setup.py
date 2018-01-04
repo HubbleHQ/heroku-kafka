c8da1188baf62380667cdb287ca29ba92cd0ae36
Lizzie <lizzie@hubblehq.com>
2018-01-04 17:47:05 +0000

    Faffing with placement of module folder

--- setup.py
+++ setup.py
@@ -1,22 +1,33 @@
-from setuptools import setup
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    version='1.0',            # Update the version number for new releases
+    version='1.0.0',            # Update the version number for new releases
     name='heroku-kafka',        # This is the name of your PyPI-package.
-    description='Python 2 & 3 kafka package for use with heroku\'s kafka.',
+    description='Python kafka package for use with heroku\'s kafka.',
+    long_description=long_description,
     classifiers=[
         'Development Status :: 5 - Production/Stable'
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
     ],
     url='https://github.com/HubbleHQ/heroku-kafka',
-    author='Hubble'
-    author_email='dev@hubblehq.com'
+    author='Hubble',
+    author_email='dev@hubblehq.com',
     license='MIT',
-    scripts=['heroku_kafka']    # The name of your scipt, and also the command you'll be using for calling it
+    py_modules=["heroku_kafka"],
+    install_requires=[
+        'kafka-python==1.3.5',
+    ]
 )
