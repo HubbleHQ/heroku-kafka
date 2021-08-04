08577973c2630762b43f2ba0a513951f65786691
Simon Andrews <simon@hubblehq.com>
2021-08-04 11:32:33 +0100

    feat: Remove Python 2.7 support from setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Intended Audience :: Developers",
     ],
